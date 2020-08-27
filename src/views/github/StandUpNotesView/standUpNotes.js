7095ca93b4cddb2d787992d251b9ce15d3316346
Saurav M. Hiremath <sauravhiremath@gmail.com>
2020-08-27 11:43:22 +0530

    update: Markdown UI with color scheme

--- src/views/github/StandUpNotesView/standUpNotes.js
+++ src/views/github/StandUpNotesView/standUpNotes.js
@@ -1,33 +1,37 @@
 import React, { useState, useEffect } from 'react';
+import clsx from 'clsx';
 import {
   Box,
   Divider,
   Card,
   Grid,
   CardContent,
-  CardHeader
+  CardHeader,
+  makeStyles
 } from '@material-ui/core';
 import ReactMde from 'react-mde';
-import { Converter as ShowdownConverter } from 'showdown';
-import 'react-mde/lib/styles/css/react-mde-all.css';
 import ReactMarkdown from 'react-markdown';
 
+import markdownOverrides from './markdownOverrides';
 import initData from './data';
 import yesterdayNotes from '../../../scripts/notesGenerator';
 import { githubQuery } from '../../../scripts/githubAPI';
 
-const converter = new ShowdownConverter({
-  tables: true,
-  simplifiedAutoLink: true,
-  strikethrough: true,
-  tasklists: true,
-  emoji: true,
-  smoothLivePreview: true,
-  ghMentions: true
+// TODO: Custom Buttons
+// const buttonPropsOptions = {
+//   writeButton:
+//   previewButton:
+//   textArea:
+// }
+
+const useStyles = makeStyles(markdownOverrides, {
+  name: 'MarkdownElement',
+  flip: false
 });
 
-const StandUpNotes = props => {
+const StandUpNotes = ({ className, ...rest }) => {
   const [value, setValue] = useState(initData);
+  const classes = useStyles();
 
   const [selectedTab, setSelectedTab] = useState('write');
   const currentTime = new Date();
@@ -53,17 +57,25 @@ const StandUpNotes = props => {
                 onChange={setValue}
                 selectedTab={selectedTab}
                 onTabChange={setSelectedTab}
-                minEditorHeight={props.height ?? 650}
-                minPreviewHeight={props.height ?? 650}
+                minEditorHeight={rest.height ?? 650}
+                minPreviewHeight={rest.height ?? 650}
                 generateMarkdownPreview={markdown =>
-                  Promise.resolve(converter.makeHtml(markdown))
+                  Promise.resolve(
+                    <ReactMarkdown
+                      source={markdown}
+                      className={clsx(classes.root, 'markdown-body', className)}
+                    />
+                  )
                 }
               />
             </Grid>
             <Grid item lg={6} sm={6} xl={6} xs={12}>
               <Card>
                 <CardContent>
-                  <ReactMarkdown source={value} />
+                  <ReactMarkdown
+                    source={value}
+                    className={clsx(classes.root, 'markdown-body', className)}
+                  />
                 </CardContent>
               </Card>
             </Grid>
